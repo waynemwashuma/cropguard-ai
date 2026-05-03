4f7e1ebce28ee81938d66ae2a6a1b4ef37c9298b
Collins Odhiambo <Collins Odhiambo>
2026-05-03 08:52:17 +0300

    Fixed title name mismatch in generated png training file

--- model/plotter.py
+++ model/plotter.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 def plot_history(csv_path):
     try:
         data = pd.read_csv(csv_path)
@@ -11,25 +12,34 @@ def plot_history(csv_path):
     fig, ax1 = plt.subplots(figsize=(10, 6))
 
     # Plot Accuracy
-    ax1.set_xlabel('Epoch')
-    ax1.set_ylabel('Accuracy (%)', color='tab:blue')
-    ax1.plot(data['Epoch'], data['Train_Acc'], label='Train Acc', color='tab:blue', linestyle='--')
-    ax1.plot(data['Epoch'], data['Val_Acc'], label='Val Acc', color='tab:blue', linewidth=2)
-    ax1.tick_params(axis='y', labelcolor='tab:blue')
-    ax1.legend(loc='upper left')
+    ax1.set_xlabel("Epoch")
+    ax1.set_ylabel("Accuracy (%)", color="tab:blue")
+    ax1.plot(
+        data["Epoch"],
+        data["Train_Acc"],
+        label="Train Acc",
+        color="tab:blue",
+        linestyle="--",
+    )
+    ax1.plot(
+        data["Epoch"], data["Val_Acc"], label="Val Acc", color="tab:blue", linewidth=2
+    )
+    ax1.tick_params(axis="y", labelcolor="tab:blue")
+    ax1.legend(loc="upper left")
     ax1.grid(True, alpha=0.3)
 
     # Plot Loss on a secondary Y-axis
-    ax2 = ax1.twinx()  
-    ax2.set_ylabel('Loss', color='tab:red')  
-    ax2.plot(data['Epoch'], data['Train_Loss'], label='Train Loss', color='tab:red')
-    ax2.tick_params(axis='y', labelcolor='tab:red')
-    ax2.legend(loc='upper right')
+    ax2 = ax1.twinx()
+    ax2.set_ylabel("Loss", color="tab:red")
+    ax2.plot(data["Epoch"], data["Train_Loss"], label="Train Loss", color="tab:red")
+    ax2.tick_params(axis="y", labelcolor="tab:red")
+    ax2.legend(loc="upper right")
 
-    plt.title('Training History: ResNet50 on Food-101')
+    plt.title("Training History: MobileNetV2 on PlantVillage Maize dataset")
     plt.tight_layout()
-    plt.savefig("training_log.png", dpi=150, bbox_inches='tight')
+    plt.savefig("training_log.png", dpi=150, bbox_inches="tight")
     plt.show()
+
 
 # Run it
 plot_history("training_log.csv")
